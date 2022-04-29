5d5068cae8ff712a18c13151da5f7936976b5b07
0Apexx0 <adityaapexx@gmail.com>
2022-04-29 18:01:22 +0530

    Updates

---
+++ precache-manifest.22dce168b8b80cf0ef079eaddaf93d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05d0da65ee4ccff4352d7ca1d63e934",
+    "url": "/React-ecommerce-website/index.html"
+  },
+  {
+    "revision": "d38df9f7f01759b415a0",
+    "url": "/React-ecommerce-website/static/css/main.d3fe06cf.chunk.css"
+  },
+  {
+    "revision": "a8f2a3a2d074c52989a5",
+    "url": "/React-ecommerce-website/static/js/2.9ebce455.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-ecommerce-website/static/js/2.9ebce455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38df9f7f01759b415a0",
+    "url": "/React-ecommerce-website/static/js/main.1b3a85c9.chunk.js"
+  },
+  {
+    "revision": "f1dfdcc09132ec2779f2",
+    "url": "/React-ecommerce-website/static/js/runtime-main.c6512282.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-ecommerce-website-/precache-manifest.3d61614c5a9113744e88d95dc65aaa77.js"
+  "/React-ecommerce-website/precache-manifest.22dce168b8b80cf0ef079eaddaf93d65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-ecommerce-website-/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-ecommerce-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
