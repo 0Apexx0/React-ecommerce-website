d67fc2e34a4ce6c18aa3f0b45928313d117d07da
0Apexx0 <adityaapexx@gmail.com>
2022-04-29 17:52:55 +0530

    Updates

---
+++ precache-manifest.3d61614c5a9113744e88d95dc65aaa77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af485cb445fecb8936f53eddcd8d3f52",
+    "url": "/React-ecommerce-website-/index.html"
+  },
+  {
+    "revision": "5650963e2a7c7c757e1b",
+    "url": "/React-ecommerce-website-/static/css/main.d3fe06cf.chunk.css"
+  },
+  {
+    "revision": "a8f2a3a2d074c52989a5",
+    "url": "/React-ecommerce-website-/static/js/2.9ebce455.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-ecommerce-website-/static/js/2.9ebce455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5650963e2a7c7c757e1b",
+    "url": "/React-ecommerce-website-/static/js/main.48f8b661.chunk.js"
+  },
+  {
+    "revision": "ef9dddc8d940f87c0685",
+    "url": "/React-ecommerce-website-/static/js/runtime-main.149f457e.js"
+  }
+]);
